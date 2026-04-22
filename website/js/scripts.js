8b4bd1de1710a406f855bd568ce1c9f70d23aa7e
evkirk <evkirk@byu.edu>
2026-04-22 15:24:32 -0600

    Add files via upload

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -3,5 +3,28 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+window.addEventListener("DOMContentLoaded", function () {
+  if (!document.body) return;
+
+  var backToTopBtn = document.createElement("button");
+  backToTopBtn.type = "button";
+  backToTopBtn.className = "back-to-top-btn";
+  backToTopBtn.setAttribute("aria-label", "Back to top");
+  backToTopBtn.textContent = "Back to top";
+
+  function toggleBackToTopVisibility() {
+    if (window.scrollY > 220) {
+      backToTopBtn.classList.add("is-visible");
+    } else {
+      backToTopBtn.classList.remove("is-visible");
+    }
+  }
+
+  backToTopBtn.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+  document.body.appendChild(backToTopBtn);
+  window.addEventListener("scroll", toggleBackToTopVisibility, { passive: true });
+  toggleBackToTopVisibility();
+});
